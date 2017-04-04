6731ee4a01b578dd3d38fe5049e03fed4dd8d7d1
Skyler H <skylerhiscock@gmail.com>
2017-04-04 17:33:51 -0500

    Changed some visual things on leaderboards

--- SpeedTyper/SpeedTyperDataObjects/User.cs
+++ SpeedTyper/SpeedTyperDataObjects/User.cs
@@ -1,12 +1,17 @@
-﻿namespace SpeedTyper.DataObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpeedTyper.DataObjects
 {
     public class User
     {
         public int UserID { get; set; }
         public string UserName { get; set; }
+        [Display(Name = "Display Name")]
         public string DisplayName { get; set; }
+        [Display(Name = "Rank")]
         public int RankID { get; set; }
         public int Level { get; set; }
+        [Display(Name = "Earned XP")]
         public int CurrentXP { get; set; }
         public int XPToLevel { get; set; }
         public bool IsGuest { get; set; }
