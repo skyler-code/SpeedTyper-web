14cf14bf2aecdc194fa3b8a872ed0a86293eba7e
Skyler H <skylerhiscock@gmail.com>
2017-04-08 18:56:24 -0500

    Starting work on test taking

---
+++ SpeedTyper/SpeedTyper.WebUI/Controllers/TestTakerController.cs
@@ -0,0 +1,25 @@
+﻿using SpeedTyper.LogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpeedTyper.WebUI.Controllers
+{
+    public class TestTakerController : Controller
+    {
+        ITestManager _testManager;
+        public TestTakerController(ITestManager testManager)
+        {
+            _testManager = testManager;
+        }
+
+        // GET: TestTaker
+        public ActionResult DisplayTest()
+        {
+            var test = _testManager.RetrieveRandomTest();
+            return View(test);
+        }
+    }
+}
