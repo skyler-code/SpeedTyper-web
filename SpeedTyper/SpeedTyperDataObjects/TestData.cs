4c514c7f8ea366dc7aa48642e1a917943b4fe870
Skyler H <skylerhiscock@gmail.com>
2017-04-08 19:13:54 -0500

    Added data source to test

--- SpeedTyper/SpeedTyperDataObjects/TestData.cs
+++ SpeedTyper/SpeedTyperDataObjects/TestData.cs
@@ -1,9 +1,12 @@
-﻿namespace SpeedTyper.DataObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpeedTyper.DataObjects
 {
     public class TestData
     {
         public int TestID { get; set; }
         public string TestDataText { get; set; }
+        [Display(Name="Source")]
         public string DataSource { get; set; }
     }
 }
