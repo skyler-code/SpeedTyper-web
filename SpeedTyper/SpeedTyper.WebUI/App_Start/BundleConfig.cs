19b6067442fb98098dd7470ebda56a5f9ec7f299
Skyler H <skylerhiscock@gmail.com>
2017-04-11 21:53:23 -0500

    Added SignalR
    Also added several things to make debugging easier

--- SpeedTyper/SpeedTyper.WebUI/App_Start/BundleConfig.cs
+++ SpeedTyper/SpeedTyper.WebUI/App_Start/BundleConfig.cs
@@ -1,4 +1,8 @@
-﻿using System.Web;
+﻿using SpeedTyper.WebUI.Infrastructure;
+using System;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
 using System.Web.Optimization;
 
 namespace SpeedTyper.WebUI
@@ -22,10 +26,34 @@ namespace SpeedTyper.WebUI
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+#if DEBUG
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css").WithLastModifiedToken());
+#else
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+#endif
+        }
+    }
+    internal static class BundleExtensions
+    {
+        public static Bundle WithLastModifiedToken(this Bundle sb)
+        {
+            sb.Transforms.Add(new LastModifiedBundleTransform());
+            return sb;
+        }
+        public class LastModifiedBundleTransform : IBundleTransform
+        {
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                foreach (var file in response.Files)
+                {
+                    var lastWrite = File.GetLastWriteTime(HostingEnvironment.MapPath(file.IncludedVirtualPath)).Ticks.ToString();
+                    file.IncludedVirtualPath = string.Concat(file.IncludedVirtualPath, "?v=", lastWrite);
+                }
+            }
         }
     }
 }
--- SpeedTyper/SpeedTyperDataAccess/DBConnection.cs
+++ SpeedTyper/SpeedTyperDataAccess/DBConnection.cs
@@ -6,8 +6,13 @@ namespace SpeedTyper.DataAccess
     {
         internal static SqlConnection GetConnection()
         {
-            //var connString = @"Data Source=localhost;Initial Catalog=SpeedTyperDB;Integrated Security=True";
-            var connString = @"Server=tcp:speedtyper.database.windows.net,1433;Initial Catalog=SpeedTyperDB;Persist Security Info=False;User ID=reader;Password=SpeedPass1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string connString;
+#if DEBUG
+            connString = @"Data Source=localhost;Initial Catalog=SpeedTyperDB;Integrated Security=True";
+#else
+
+            connString = @"Server=tcp:speedtyper.database.windows.net,1433;Initial Catalog=SpeedTyperDB;Persist Security Info=False;User ID=reader;Password=SpeedPass1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+#endif
             var conn = new SqlConnection(connString);
             return conn;
         }
