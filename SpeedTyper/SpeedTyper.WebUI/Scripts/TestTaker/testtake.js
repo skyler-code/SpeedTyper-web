9dccdecc24a17fdb6099f86164f1666efce18b96
Skyler H <skylerhiscock@gmail.com>
2017-04-12 22:27:12 -0500

    You can now see the words you've gotten correct

--- SpeedTyper/SpeedTyper.WebUI/Scripts/TestTaker/testtake.js
+++ SpeedTyper/SpeedTyper.WebUI/Scripts/TestTaker/testtake.js
@@ -2,9 +2,11 @@ console.log("js loaded");
 
 
 var testDataList;
+var correctWords = [];
 
 function testInitialize() {
-    testDataList = document.getElementById("test-data-text").innerHTML.split(/(\s+)/);
+    testDataList = document.getElementById("untyped-words").innerHTML.split(/\b(?![\s.])/);
+    console.log(testDataList);
 }
 
 function processInput() {
@@ -12,12 +14,14 @@ function processInput() {
         testInitialize();
     }
     var userInput = document.getElementById("txtTextEntryBox");
-    var testData = document.getElementById("test-data-text");
+    var untypedWords = document.getElementById("untyped-words");
+    var correctsWordsDisplay = document.getElementById("correct-words");
     var firstWord = testDataList[0];
-    if(userInput.value == firstWord){
+    if (userInput.value == firstWord) {
+        correctWords.push(testDataList.shift());
         userInput.value = "";
-        testDataList.shift();
-        testData.innerHTML = testDataList.join("");
+        untypedWords.innerHTML = testDataList.join("");
+        correctsWordsDisplay.innerHTML = correctWords.join("");
     }
     
 }
