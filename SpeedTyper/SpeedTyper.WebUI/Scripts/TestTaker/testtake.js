a59aea62eebe4ea7472c8a5527ea1ec76fb81997
Skyler H <skylerhiscock@gmail.com>
2017-04-28 00:02:42 -0500

    unneccesary param

--- SpeedTyper/SpeedTyper.WebUI/Scripts/TestTaker/testtake.js
+++ SpeedTyper/SpeedTyper.WebUI/Scripts/TestTaker/testtake.js
@@ -74,7 +74,7 @@ $(function () {
         $('#txtTextEntryBox').val('Test over.').prop('readonly', true).blur();
         var _guid = $('#guid').html();
         _timeElapsed = (Date.now() - startTime) * (1 / 1000);
-        proxy.invoke('submitTest', _testID, CalculateWPM(), parseInt(_timeElapsed), endTimerCountdown, correctWords.join(""), dataSource, startTime.toString(), stH);
+        proxy.invoke('submitTest', _testID, CalculateWPM(), parseInt(_timeElapsed), endTimerCountdown, correctWords.join(""), startTime.toString(), stH);
     }
 
     connection.start().done(function (e) {
